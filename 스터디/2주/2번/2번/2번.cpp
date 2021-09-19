dadbf0c0b956ff98d00274fb58449fecbf5f9d5f
djelalswns12 <djelalswns12@naver.com>
2021-09-19 21:41:14 +0900

    09.18 알고리즘 스터디

--- 스터디/2주/2번/2번/2번.cpp
+++ 스터디/2주/2번/2번/2번.cpp
@@ -7,10 +7,13 @@
 using namespace std;
 vector<int> list;
 set<double> s;
-void solution(int N) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-           // cout << "i:"<<i<<"--"<<(j + 1) / (double)(i + 1)<<"\n";
+void solution(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            //cout << "i:"<<i<<"--"<<(j + 1) / (double)(i + 1)<<"\n";
             s.insert((j + 1) / (double)(i + 1));
         }
         list.push_back((s.size() * 2) + 3);
@@ -24,12 +27,13 @@ int main()
     vector<int> answer;
     int d;
     solution(1000);
-    for (int i = 0; i < cnt; i++) {
+    for (int i = 0; i < cnt; i++)
+    {
         cin >> d;
-        answer.push_back(list.at(d-1));
+        answer.push_back(list.at(d - 1));
     }
-    for (int d : answer) {
+    for (int d : answer)
+    {
         cout << d << "\n";
     }
 }
-
