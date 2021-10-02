e5072a6fb2883ccf2eed39aa7f1a161fadf20dfb
djelalswns12 <djelalswns12@naver.com>
2021-10-03 03:29:36 +0900

    3주차 스터티 못푼 문제 풀이

--- 스터디/3주/문제3/문제3/문제3.cpp
+++ 스터디/3주/문제3/문제3/문제3.cpp
@@ -1,26 +1,57 @@
 ﻿
 #include <iostream>
 #include <vector>
+#include <set>
 #include <queue>
 using namespace std;
+
+//투포인터로 접근해야함
+
+struct cmp
+{
+	bool operator()(int n1,int n2) {
+		if (abs(n1) > abs(n2)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+};
+//14921번 용액합성하기
 int main()
 {
-	int now_cnt, add_cnt, road;
-	int num;
-	priority_queue<int, vector<int>> q;
-	cin >> now_cnt >> add_cnt >> road;
-	for (int i = 0; i < now_cnt; i++) {
+	multiset<int> s;
+	priority_queue<int, vector<int>,cmp> q;
+	multiset<int, greater<>> s2;
+	int cnt, num;
+	cin >> cnt;
+	for (int i = 0; i < cnt; i++) {
 		cin >> num;
-		q.push(num);
+		s.insert(num);
+		s2.insert(num);
 	}
-	int len=0;
-	int max = q.top();
-	q.pop();
-	while (!q.empty()) {
-		if (len <= max - q.top()) {
-			len = max - q.top();
+	int less;//내림차순
+	int greeter;//오름차순
+	for (auto ori = s.begin(); ori != s.end(); ori++) {
+		auto iter = s.lower_bound(-(*ori));
+		if (iter == s.end()) {
+			iter--;
 		}
-		q.pop();
-		max = q.top();
+		if (iter != ori) {
+			//cout << "insert" << (*iter) << "/" << (*ori) << "\n";
+			q.push((*iter) + (*ori));
+		}
 	}
+	for (auto ori = s2.begin(); ori != s2.end(); ori++) {
+		auto iter = s2.lower_bound(-(*ori));
+		if (iter == s2.end()) {
+			iter--;
+		}
+		if (iter != ori) {
+			//cout << "insert" << (*iter) << "/" << (*ori) << "\n";
+			q.push( (*iter) + (*ori));
+		}
+	}
+	cout<<q.top();
 }
---
+++ 스터디/3주/문제4/문제4/문제4/문제4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <string>
+using namespace std;
+//1477번 휴게소 세우기
+#define maxsort(x) sort(x.begin(),x.end(),greater<int>()); // 내림차순
+#define minsort(x) sort(x.begin(),x.end()); // 오름차순
+
+using ll = long long;
+using pqmax = priority_queue<int, vector<int>, less<int>>;//Top is MAX
+using pqmin = priority_queue<int, vector<int>, greater<int>>;//Top is MIN
+using mapmax = map<int, int, greater<int>>;//Top is MAX
+using mapmin = map<int, int, less<int>>;//Top is MIN
+using setmax = set<int,greater<int>>;//Top is MAX
+using setmin = set<int>;//Top is MIN
+
+int main()
+{
+	
+	int now_cnt, add_cnt, road;
+	int num;
+	vector<int> v;
+	cin >> now_cnt >> add_cnt >> road;
+	v.push_back(0);
+	v.push_back(road);
+	for (int i = 0; i < now_cnt; i++) {
+		cin >> num;
+		v.push_back(num);
+	}
+	minsort(v);
+	int left = 0;
+	int right = road;
+	while (left <= right) {
+	
+		int mid = (left + right) / 2;
+		int cur = 0;
+		for (int i = 0; i < v.size()-1; i++) {
+			int set = (v.at(i + 1) - v.at(i));
+			cur += set / mid;
+			if (set % mid == 0) {
+				cur--;
+			}	
+		}
+		if (cur <= add_cnt) {
+			right = mid - 1;
+		}
+		else {
+			left = mid + 1;
+		}
+	}
+	cout << left;
+}
