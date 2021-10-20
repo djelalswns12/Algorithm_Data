e866800be255ccfec5b50aff6ceeac032130d5ee
djelalswns12 <djelalswns12@naver.com>
2021-10-20 22:04:58 +0900

    백트래킹 풀이2
    DFS로 접근
    
    순열 - 요소의 위치에 따라 객체로 인식
    >>DFS의 탐색인덱스를 0부터 시작하여 전체 수행하면 된다.
    >>중복이 불가능하므로 이미 방문했던 요소에 대해서는 visit처리를 한다
    
    중복순열 - 숫자의 중복이 가능한 순열
    >>순열에서 visit을 제외하면 구현가능
    
    조합 - 요소가 같다면 같은 객체
    >>탐색인덱스를 매개변수로 넘겨서 이전 요소들이 배제되도록 한다
    
    중복조합 - 요소의 중복이 가능한 조합
    >>조합에서 visit을 제외하면 구현가능

---
+++ 분류별/백트래킹/15651번 N과 M (3)/15651번 N과 M (3)/15651번 N과 M (3).cpp
@@ -0,0 +1,37 @@
+﻿// 15651번 N과 M (3).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void dfs() {
+
+}
+vector<int> v;
+vector<bool> visit;
+void dfs(vector<int> &answer,int size) {
+    if (answer.size() >= size) {
+        for (int d : answer) {
+            cout << d << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for (int i = 0; i < v.size(); i++) {
+        answer.push_back(v.at(i));
+        dfs(answer,size);
+        answer.pop_back();
+    }
+}
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        v.push_back(i);
+        visit.push_back(false);
+    }
+    vector<int> answer;
+    dfs(answer,M);
+}
